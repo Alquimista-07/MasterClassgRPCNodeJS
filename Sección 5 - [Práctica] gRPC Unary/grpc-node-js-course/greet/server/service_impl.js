b1a1e6092e29e70f8fea50c0b76be08d80404055
Alquimista-07 <jdca.07@gmail.com>
2023-09-05 16:05:58 -0500

    22. Implementación de cliente API unario

--- Sección 5 - [Práctica] gRPC Unary/grpc-node-js-course/greet/server/service_impl.js
+++ Sección 5 - [Práctica] gRPC Unary/grpc-node-js-course/greet/server/service_impl.js
@@ -3,7 +3,7 @@ const pb = require('../proto/greet_pb');
 exports.greet = (call, callback) => {
     console.log("Greet was invoked");
     const res =  new pb.GreetResponse()
-        .setResult(`Hello ${call.request.getFirstName()}`);
+      .setResult(`Hello ${call.request.getFirstName()}`);
 
     callback(null, res);
 }
