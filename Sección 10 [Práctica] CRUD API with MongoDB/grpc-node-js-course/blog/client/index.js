38f7c2aa62bc9573eea588d0fdbb51f5a1437241
Alquimista-07 <jdca.07@gmail.com>
2023-09-06 15:34:06 -0500

    48. CreateBlog Client

--- Sección 10 [Práctica] CRUD API with MongoDB/grpc-node-js-course/blog/client/index.js
+++ Sección 10 [Práctica] CRUD API with MongoDB/grpc-node-js-course/blog/client/index.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 const grpc = require('@grpc/grpc-js');
 const {BlogServiceClient} = require('../proto/blog_grpc_pb');
+const {Blog, BlogId} = require('../proto/blog_pb');
 
-function main() {
+function createBlog(client) {
+    console.log('---createBlog was invoked---');
+
+    return new Promise((resolve, reject) => {
+        const req = new Blog()
+          .setAuthorId('Clement')
+          .setTitle('My First Blog')
+          .setContent('Content of the first blog');
+
+        client.createBlog(req, (err, res) => {
+            if (err) {
+                reject(err);
+            }
+
+            console.log(`Blog was created: ${res}`);
+            resolve(res.getId());
+        });
+    });
+}
+
+async function main() {
     const tls = true;
     let creds;
 
@@ -14,6 +35,8 @@ function main() {
         creds = grpc.ChannelCredentials.createInsecure();
     }
     const client = new BlogServiceClient('localhost:50051', creds);
+
+    const id = await createBlog(client);
 
     client.close();
 }
